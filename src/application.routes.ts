f6368ee04e7bc03e54a9a05ecc27126b9ba5dc4c
Harshil Kotadiya <harshilkotadiya002@gmail.com>
2023-10-23 19:19:32 +0530

    third commit

--- src/application.routes.ts
+++ src/application.routes.ts
@@ -2,10 +2,8 @@ import { Application } from "express";
 import AdministrationRoutes from "./components/administration/administration.routes";
 import StudentRoutes from "./components/student/student.routes";
 
-
 export default class ApplicationConfig {
   public static registerRoute(app: Application) {
-
     app.use("/student", StudentRoutes);
 
     app.use("/administration", AdministrationRoutes);
