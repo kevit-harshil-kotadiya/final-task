7f51b50901d9d1386dac601479404ec75036a457
Harshil Kotadiya <harshilkotadiya002@gmail.com>
2023-10-25 12:44:32 +0530

    removed unnecessary comments

--- __tests__/index-test.ts
+++ __tests__/index-test.ts
@@ -5,26 +5,15 @@ const mongoose = require("mongoose");
 const express = require("express");
 async function f() {
   await mongoose.connect(Config.mongodb.url);
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 f();
 
 export const app = express();
 
-app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-    parameterLimit: 50000,
-  })
-);
-app.use(express.static("public"));
-app.use(bodyParser.json());
+
 app.use(express.json());
 
 ApplicationConfig.registerRoute(app);
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4comment below lines and run npm test to start testing
 
-////$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 export default app;
--- jest.config.js
+++ jest.config.js
@@ -1,46 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-// export const preset = 'ts-jest'
-// export const testEnvironment = 'node'
-// export const clearMocks = true
-// export const coverageProvider = 'v8'
-// export const moduleFileExtensions = ['js', 'jsx', 'ts', 'tsx', 'json', 'node']
-// export const roots = ['FINAL-TASK/src']
-// export const testMatch = [
-//     '**/__tests__/**/*.[jt]s?(x)',
-//     '**/?(*.)+(spec|test).[tj]s?(x)',
-// ]
-// export const transform = {
-//     '^.+\\.(ts|tsx)$': 'ts-jest',
-// }
-////////////////////////////////////////////////////////////////////////
-// module.exports = {
-//     preset: 'ts-jest',
-//     testEnvironment: 'node',
-//     clearMocks: true,
-//     coverageProvider: 'v8',
-//     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
 
-//     roots: [],
-
-//     testMatch: [
-//         '**/__tests__/**/*.[jt]s?(x)',
-//         '**/?(*.)+(spec|test).[tj]s?(x)',
-//     ],
-//     transform: {
-//         '^.+\\.(ts|tsx)$': 'ts-jest',
-//     },
-// }
-///////////////////////////////////////////////////////////////////////////////
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/**/*.test.ts"],
   verbose: true,
   forceExit: true,
-  // transformIgnorePatterns: [
-  //     // By default, ignore files in node_ modules
-  //     '/home/kevit/Desktop/VSCODE/node-original/my/Final-Task/__tests__/user.test.ts',
-  // ],
+
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
